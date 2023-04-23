1e7cc27041599741dc2c7cb876790db649c1c2c8
HIMENOWORKS <himeno.workspace@gmail.com>
2023-04-23 08:57:21 +0700

    create dialog

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,7 @@
+const Dialog = () => {
+   return (
+      <dialog className="z-50 top-0 left-0 w-full h-full backdrop:bg-opacity-50 open:animate-fade-in backdrop-blur-sm bg-black/30"></dialog>
+   );
+};
+
+export default Dialog;
