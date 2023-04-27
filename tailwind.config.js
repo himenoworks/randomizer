6153604ea7e0d014dfdc44697201b44e55b7d435
HIMENOWORKS <himeno.workspace@gmail.com>
2023-04-27 11:39:22 +0700

    add fade out animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
             "zoom-in": "zoom-in 0.2s ease-in-out",
             "zoom-out": "zoom-out 0.2s ease-in-out",
             "fade-in": "fade-in 0.2s ease-out",
+            "fade-out": "fade-out 0.2s ease-out",
          },
          keyframes: {
             "zoom-in": {
