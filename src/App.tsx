0f44854e1bd1be1c74878cc3ca936bd728c4e0fd
HIMENOWORKS <himeno.workspace@gmail.com>
2023-04-28 23:21:16 +0700

    add font awesome package

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FC } from "react";
 
 import ApplicationRoute from "./routes";
@@ -7,3 +11,4 @@ const App: FC = () => {
 };
 
 export default App;
+library.add(fab, fas, far);
