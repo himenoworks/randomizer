42caafbb90d2dfda7cd9d4d544d76174c4cea0fa
Aiyaruch <aiyaruch1320@hotmail.com>
2023-05-03 21:51:34 +0700

    refactor random
    - change way to shuffle array to use Fisher-Yates shuffle algorithm
    - change way to map name group and fuction name

--- src/helper/random.ts
+++ src/helper/random.ts
@@ -1,22 +1,24 @@
 import { Group } from "../pages/RandomGroup";
 
-const shuffleArray = (array: string[]): string[] => {
-   return array.sort(() => Math.random() - 0.5);
+const shuffleArray = <T>(array: T[]): T[] => {
+   const shuffled = [...array];
+   for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+   }
+   return shuffled;
 };
 
-const mapNameGroup = (groupNames: string[]): Group => {
-   const nameRecords: Group = {};
-   groupNames.forEach((nameOfGroup) => (nameRecords[nameOfGroup] = []));
-   return nameRecords;
-};
+const createEmptyGroups = (groupNames: string[]): Group =>
+   Object.fromEntries(groupNames.map((groupName) => [groupName, []]));
 
 export const randomGroup = (members: string[], groupNames: string[]): Group => {
-   const oldSortedGroups = mapNameGroup(groupNames);
+   const oldSortedGroups = createEmptyGroups(groupNames);
    const shuffledMembers = shuffleArray(members);
    const shuffledGroups = shuffleArray(groupNames);
-   const newSortedGroups = mapNameGroup(shuffledGroups);
-   const membersPerGroups = Math.ceil(members.length / groupNames.length);
-   for (let i = 0; i < membersPerGroups; i++) {
+   const newSortedGroups = createEmptyGroups(shuffledGroups);
+   const membersPerGroup = Math.ceil(members.length / groupNames.length);
+   for (let i = 0; i < membersPerGroup; i++) {
       shuffledGroups.forEach((nameOfGroup) => {
          shuffledMembers.length !== 0 &&
             newSortedGroups[nameOfGroup].push(shuffledMembers.shift()!);
